151e462d348e66e06d91e92e99b28907cc0d4709
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-02-14 21:20:50 -0600

    updating files

--- vm-fiware-orion/init.sh
+++ vm-fiware-orion/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "\n" >> /home/vagrant/.bashrc
-echo 'export PS1="\[\e[01;34m\]\u\[\e[0m\]\[\e[01;37m\]:\H:\w\[\e[0m\]\[\e[00;37m\]\n\\$ \[\e[0m\]"' >> /home/vagrant/.bashrc
+echo 'export PS1="\[\e[01;34m\]orion-context-broker\[\e[0m\]\[\e[01;37m\]:\w\[\e[0m\]\[\e[00;37m\]\n\\$ \[\e[0m\]"' >> /home/vagrant/.bashrc
 printf "\n" >> /home/vagrant/.bashrc
 
 
